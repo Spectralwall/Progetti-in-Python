1a771e1b4efe001d667c7772b14b45da77891c38
Gabriele <gabrinaretto@libero.it>
2021-01-21 20:52:33 +0100

    Fine mini programma per salvare film
    finito

--- Utility/Film Save/FilmList.py
+++ Utility/Film Save/FilmList.py
@@ -7,34 +7,54 @@ def spezza(frase):
     None
 
 class filmClass:
-    def __init__(self):#costruttore della classe film
+    def __init__(self,voto,nome):#costruttore della classe film
         self.votoC = voto
         self.nomeC = nome
 
+    def __repr__(self):
+        return self.writeFilm()
+
+    def __str__(self):
+        return self.writeFilm()
+
     def writeFilm(self):
-        return self.nomeC +" ,"+ self.votoC
+        return self.nomeC +","+ self.votoC
 
 print("Che operazione vuoi svolgere ?")
 print("1) inserire un nuovo film ?")
 print("2) vedere la lista dei film")
-scelta = input()
+print("3) terminare l'app")
 
-if(scelta == "1"):
-    nome = input("Come si chiama il film : ")
-    voto = input("Che voto gli dai : ")
-    film = filmClass()#prendo i dati e creo un oggetto film
+while(True):
+    scelta = input()
+    if(scelta == "1"):
+        nome = input("Come si chiama il film : ")
+        voto = input("Che voto gli dai : ")
+        film = filmClass(voto,nome)#prendo i dati e creo un oggetto film
 
-    #scrittura su file
-    writer = open("Film","a")#file dove scriver e tipo di scrittura(a append to file , w overwrite file)
-    writer.write(film.writeFilm())
-    writer.close()
-    None
-elif(scelta == "2"):
-    Films = []#lista dei film
-    reader = open("Film","r")#leggo da file
-    l = reader.readlines()
-    for x in l:
+        #scrittura su file
+        writer = open("Film","a")
+        writer.write("\n" + film.writeFilm())
+        writer.close()
 
-    None
-else:
-    print("scelta non riconosciuta")
+        print("vuoi fare qualcos'altro ?")
+    elif(scelta == "2"):
+        Films = []#lista dei film
+        reader = open("Film","r")#leggo da file
+        l = reader.readlines()
+        for x in l:
+            nome,voto = x.split(",")
+            f = filmClass(voto,nome)
+            Films.append(f)
+
+        Films.sort(key=lambda x: x.votoC)#ordino per voto
+        for x in Films:
+            print(x)
+
+        print("vuoi fare qualcos'altro ?")
+    elif(scelta == "3"):
+        print("Goodbye")
+        break
+    else:
+        print("scelta non riconosciuta")
+None
